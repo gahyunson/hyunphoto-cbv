c1a0c31b0516cf4d3f6b7c0001de3ca02244927f
GH <chelsey.gahyun@gmail.com>
2024-12-15 21:40:11 +0900

    add comment

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,3 +1,6 @@
+"""
+Configure the global URLconf.
+"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -9,6 +12,7 @@ from drf_spectacular.views import (
 )
 
 urlpatterns = [
+    # path(route, view),
     path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),
     path('api/photos/', include('photos.urls')),
